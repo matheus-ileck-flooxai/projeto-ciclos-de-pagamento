e2fd889800e900fbd2a8b5fbe336268d6d10eef5
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-23 12:39:31 -0300

    feat: add midleware to handle errors

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,10 @@
 const billingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
+
 
 billingCycle.methods(['get', 'post', 'put', 'delete'])
 billingCycle.updateOptions({ new: true, runValidators: true })
+billingCycle.after('post', errorHandler).after('put', errorHandler)
 
 billingCycle.route('get', (req, res, next) => {
 
