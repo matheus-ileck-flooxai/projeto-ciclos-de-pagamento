431292b3b1c99c5339a653c4d263f75576f67160
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-23 11:18:43 -0300

    feat: Map API routes for billing cycle

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,24 @@
+const billingCycle = require('./billingCycle')
+
+billingCycle.methods(['get', 'post', 'put', 'delete'])
+billingCycle.updateOptions({new: true, runValidators: true})
+
+BillingCycle.route('get', (req, res, next) => {
+
+    BillingCycle.find({}, (err, docs) => {
+
+        if(!err) {
+
+            res.json(docs)
+
+        } else {
+
+            res.status(500).json({errors: [error]})
+
+        }
+
+    })
+
+})
+
+module.exports = billingCycle
