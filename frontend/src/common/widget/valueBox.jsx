6d36964a948fae8fff5fed7b6d5319ef0d87b2e3
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-24 11:37:46 -0300

    feat: using ValueBox component on Dashboard

--- frontend/src/common/widget/valueBox.jsx
+++ frontend/src/common/widget/valueBox.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Grid from '../layout/grid'
 
 export default props => (
+
     <Grid cols={props.cols}>
         <div className={`small-box bg-${props.color}`}>
             <div className='inner'>
