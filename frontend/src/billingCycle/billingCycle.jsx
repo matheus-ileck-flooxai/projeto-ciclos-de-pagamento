8f11ca4cdf877b6801c60c3bfe67c93719d573a2
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-25 10:22:12 -0300

    feat: add TabContent component and integrate BillingCycle with Redux

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { selectTab } from "../common/tab/tabActions";
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
+import TabContent from "../common/tab/tabContent";
 
 class BillingCycle extends Component {
+    componentWillMount(){
+        this.props.selectTab('tabList')
+    }
     render() {
         return (
             <div>
@@ -20,7 +26,12 @@ class BillingCycle extends Component {
                             <TabHeader label='Alterar' icon='pencil' target='tabUpdate'></TabHeader>
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete'></TabHeader>
                         </TabsHeader>
-                        <TabsContent></TabsContent>
+                        <TabsContent>
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+                        </TabsContent>
                     </Tabs>
                 </Content>
             </div>
@@ -29,4 +40,6 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+
+export default connect(null, mapDispatchToProps)(BillingCycle)
