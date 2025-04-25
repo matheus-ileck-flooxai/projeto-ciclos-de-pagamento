1b6451d4d3b06868db1d7176fcdf8437948ee360
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-25 13:05:35 -0300

    feat: add error messages using react-redux-toastr

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -3,12 +3,13 @@ import {reducer as formReducer} from 'redux-form'
 import dashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from "../common/tab/tabReducer";
 import BillingCycleReducer from "../billingCycle/billingCycleReducer";
-
+import {reducer as toastrReducer} from 'react-redux-toastr'
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 })
 
 export default rootReducer
