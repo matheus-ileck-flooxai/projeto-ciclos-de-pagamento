49fe109f2858617f856e6937c75586d040699e75
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-25 10:47:12 -0300

    feat: add showTabs action to control tab Header

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { selectTab } from "../common/tab/tabActions";
+import { selectTab, showTabs } from "../common/tab/tabActions";
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import Tabs from "../common/tab/tabs";
@@ -11,8 +11,9 @@ import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
 
 class BillingCycle extends Component {
-    componentWillMount(){
+    componentWillMount() {
         this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
     }
     render() {
         return (
@@ -40,6 +41,6 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch)
 
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- frontend/src/common/tab/tabContent.jsx
+++ frontend/src/common/tab/tabContent.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import If from "../operator/if";
 import { connect } from "react-redux";
 
 class TabContent extends Component {
     render() {
         const selected = this.props.tab.selected === this.props.id
+        const visible = this.props.tab.visible[this.props.id]
+
         return (
-            <div id={this.props.id} className={`tab-pane ${selected ? 'active' : ''}`}>
-                {this.props.children}
-            </div>
+            <If test={visible}>
+                <div id={this.props.id} className={`tab-pane ${selected ? 'active' : ''}`}>
+                    {this.props.children}
+                </div>
+            </If>
         )
     }
 }
