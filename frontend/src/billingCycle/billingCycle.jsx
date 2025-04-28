a45d8f735728c68a1ede4b765f5590a769dd6e26
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-28 11:50:47 -0300

    feat: use formValueSelector to get values from form

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -10,15 +10,13 @@ import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
 import List from "./billingCycleList";
 
-import { selectTab, showTabs } from "../common/tab/tabActions";
-import { create, update, remove } from "./billingCycleActions";
+import { init, create, update, remove } from "./billingCycleActions";
 
 import Form from "./billingCycleForm"
 
 class BillingCycle extends Component {
     componentWillMount() {
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabCreate')
+        this.props.init()
     }
     render() {
         return (
@@ -54,6 +52,6 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create, update, remove }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ init, create, update, remove }, dispatch)
 
 export default connect(null, mapDispatchToProps)(BillingCycle)
